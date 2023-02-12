59dfc164c851c8791675576ba8a03491c36a55da
omarRmalik <maliko@mail.gvsu.edu>
2023-02-12 16:11:13 -0500

    Updated 2/12

--- solution.py
+++ solution.py
@@ -1,22 +1,39 @@
 from collections import Counter, defaultdict
-from operator import itemgetter
 
 visits = defaultdict(list)
 
+
+# Collect places function
+
 def collect_places():
-  while True:
-    place = input('Tell me where you went: ')
-    if not place:
-      break
-    if "," not in place:
-      print("That's not a legal city, country combination")
-      continue
-    city, country = place.strip().split(",")
-    visits[country].append(city)
-  return visits
+    while True:
+        place = input('Tell me where you went: ')
+        if not place:
+            break
+        if "," not in place:
+            print("That's not a legal city, country combination")
+            continue
+        city, country = place.strip().split(",")
+        global visits
+        visits[country].append(city)
+    for key, value in sorted(visits.items()):
+        visits[key] = sorted(value)
+
+
+# Display places function
 
 def display_places():
-  output = "You visited:\n"
-  for key, value in visits.items():
-    output += f'{key[1]}\n\t{key[0]}\t({value if value > 1 else None})\n'
-  return output
+    counts_dict = defaultdict(dict)
+    for key, value in visits.items():
+        counts = Counter(value)
+        for item, count in counts.items():
+            if isinstance(item, str):
+                counts_dict[key][item] = count
+
+    output = 'You visited:\n'
+
+    for key, value in counts_dict.items():
+        output += f'{key}\n\t'
+        for city, times in value.items():
+            output += f'{city}\t ({times if times > 1 else 1})\n'
+    return output
