37eba10aae80c1a50c41e862f5ad385ea5dd54a2
omarRmalik <maliko@mail.gvsu.edu>
2023-02-17 15:02:07 -0500

    Exercise 2 solved 2-17-23

--- Exercise 2/solution.py
+++ Exercise 2/solution.py
@@ -1,6 +1,6 @@
 # myrange2 that returns a list
 
-def myrange2(y, x=0, increment=1):
+def myrange(x, y, increment):
   output = []
   current_val = x
   output.append(current_val)
@@ -8,6 +8,19 @@ def myrange2(y, x=0, increment=1):
     current_val = (current_val + increment)
     output.append(current_val)
   return output
+
+def myrange2(*args):
+  if not args:
+    raise ValueError('No arguments provided!')
+  elif len(args) == 1:
+    return myrange(0, y=args[0], increment=1)
+  elif len(args) == 2 and args[0] < args[1]:
+    return myrange(x=args[0], y=args[1], increment=1)
+  elif len(args) == 2 and args[0] > args [1]:
+    return []
+  else:
+    first, second, third = args
+    return myrange(x = first, y = second, increment = third)
 
 # myrange that returns a generator
 
