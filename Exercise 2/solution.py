5f916dccb5152724ca2ee50eb82c5e5971608d40
omarRmalik <maliko@mail.gvsu.edu>
2023-02-17 16:12:47 -0500

    Exercise 2 solved 2-17-23

--- Exercise 2/solution.py
+++ Exercise 2/solution.py
@@ -12,17 +12,17 @@ def myrange(x, y, increment):
 def myrange2(*args):
   if not args:
     raise ValueError('No arguments provided!')
-  elif len(args) == 1 and isinstance(args[0], int):
-    return myrange(x = 0, y = args[0], increment = 1)
-  elif len(args) == 3 and isinstance(args[0], int) and isinstance(args[1], None) and isinstance((args[2], None)):
-    return myrange(x=0, y = args[0], increment = 1)
-  elif len(args) == 3 and isinstance(args[0], int) and isinstance(args[1], int) and isinstance((args[2], None)) and args[0] < args[1]:
-    return myrange(x = args[0], y = args[1], increment = 1)
-  elif len(args) == 3 and isinstance(args[0], int) and isinstance(args[1], int) and isinstance((args[2], None)) and args[0] > args[1]:
-    return [ ]
-  else:
-    first, second, third = args
-    return myrange(x = first, y = second, increment = third)
+  elif len(args) >= 1:
+    non_none_args =[one_arg for one_arg in args
+                    if one_arg is not None]
+    if len(non_none_args) == 1:
+      return myrange(x = 0, y = non_none_args[0], increment = 1)
+    elif len(non_none_args) == 2 and non_none_args[0] < non_none_args[1]:
+      return myrange(x=non_none_args[0], y=non_none_args[1], increment=1)
+    elif len(non_none_args) == 3:
+      return myrange(x = non_none_args[0], y = non_none_args[1], increment = non_none_args[2])
+    else:
+      return [ ]
 
 # myrange that returns a generator
 
