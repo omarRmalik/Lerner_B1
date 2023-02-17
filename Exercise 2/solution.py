bb94fb8dfeec1dd1fe32710a6c01d108032e5b0c
omarRmalik <maliko@mail.gvsu.edu>
2023-02-17 17:48:43 -0500

    Exercise 2 solved 2-17-23

--- Exercise 2/solution.py
+++ Exercise 2/solution.py
@@ -26,9 +26,24 @@ def myrange2(*args):
 
 # myrange that returns a generator
 
-def myrange3(x, y, increment=1):
+def myrange_gen(x, y, increment=1):
   current_val = x
   yield x
   while current_val < (y - increment):
     current_val = current_val + increment
     yield current_val
+
+def myrange3(*args):
+  if not args:
+    raise ValueError('No arguments provided!')
+  elif len(args) >= 1:
+    non_none_args =[one_arg for one_arg in args
+                    if one_arg is not None]
+    if len(non_none_args) == 1:
+      return myrange_gen(x = 0, y = non_none_args[0], increment = 1)
+    elif len(non_none_args) == 2 and non_none_args[0] < non_none_args[1]:
+      return myrange_gen(x=non_none_args[0], y=non_none_args[1], increment=1)
+    elif len(non_none_args) == 3:
+      return myrange_gen(x = non_none_args[0], y = non_none_args[1], increment = non_none_args[2])
+    else:
+      return [ ]
