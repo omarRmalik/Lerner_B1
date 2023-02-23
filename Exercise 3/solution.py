0beddc8bc7f17742222305863a8e9742f50068f5
omarRmalik <maliko@mail.gvsu.edu>
2023-02-23 17:12:10 -0500

    Exercise 3 solved 2-23-23

--- Exercise 3/solution.py
+++ Exercise 3/solution.py
@@ -5,20 +5,18 @@ def logtolist(filename):
   ip_address_pattern = re.compile(r'^\d{2}.\d{2,3}.\d{2,3}.\d{2}')
   time_stamp_pattern = re.compile(r'(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})')
   request_pattern = re.compile(r'GET\s([^"]+)')
-  with open('filename', 'r') as infile:
-    lines = infile.read()
-    for one_line in lines:
-      temp_dict = {}
-      ip_address = re.match(ip_address_pattern, one_line)
-      if ip_address:
-        temp_dict['ip_address'] = ip_address.group(0)
+  for one_line in filename:
+    temp_dict = {}
+    ip_address = re.match(ip_address_pattern, one_line)
+    if ip_address:
+      temp_dict['ip_address'] = ip_address.group(0)
       time_stamp = re.search(time_stamp_pattern, one_line)
-      if time_stamp:
-        temp_dict['time_stamp'] = time_stamp.group(0)
+    if time_stamp:
+      temp_dict['time_stamp'] = time_stamp.group(0)
       request = re.search(request_pattern, one_line)
-      if request:
-        temp_dict['request'] = request.group(0)
-      output.append(temp_dict)
+    if request:
+      temp_dict['request'] = request.group(0)
+    output.append(temp_dict)
   return output
 
 
@@ -27,18 +25,16 @@ def re_logtolist(filename):
   ip_address_pattern = re.compile(r'^\d{2}.\d{2,3}.\d{2,3}.\d{2}')
   time_stamp_pattern = re.compile(r'(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})')
   request_pattern = re.compile(r'GET\s([^"]+)')
-  with open('filename', 'r') as infile:
-    lines = infile.read()
-    for one_line in lines:
-      temp_dict = {}
-      ip_address = re.match(ip_address_pattern, one_line)
-      if ip_address:
-        temp_dict['ip_address'] = ip_address.group(0)
+  for one_line in filename:
+    temp_dict = {}
+    ip_address = re.match(ip_address_pattern, one_line)
+    if ip_address:
+      temp_dict['ip_address'] = ip_address.group(0)
       time_stamp = re.search(time_stamp_pattern, one_line)
-      if time_stamp:
-        temp_dict['time_stamp'] = time_stamp.group(0)
+    if time_stamp:
+      temp_dict['time_stamp'] = time_stamp.group(0)
       request = re.search(request_pattern, one_line)
-      if request:
-        temp_dict['request'] = request.group(0)
-      output.append(temp_dict)
+    if request:
+      temp_dict['request'] = request.group(0)
+    output.append(temp_dict)
   return output
