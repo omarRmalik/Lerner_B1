29f1ef1933c2425f912d6320788a82dacb6e76a3
omarRmalik <maliko@mail.gvsu.edu>
2023-02-23 17:16:41 -0500

    Exercise 3 solved 2-23-23

--- Exercise 3/solution.py
+++ Exercise 3/solution.py
@@ -2,7 +2,7 @@ import re
 
 def logtolist(filename):
   output = [ ]
-  ip_address_pattern = re.compile(r'^\d{2}.\d{2,3}.\d{2,3}.\d{2}')
+  ip_address_pattern = re.compile(r'^\d{2}.\d{2,3}.\d{2,3}.\d{2,3}')
   time_stamp_pattern = re.compile(r'(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})')
   request_pattern = re.compile(r'GET\s([^"]+)')
   for one_line in filename:
@@ -10,10 +10,10 @@ def logtolist(filename):
     ip_address = re.match(ip_address_pattern, one_line)
     if ip_address:
       temp_dict['ip_address'] = ip_address.group(0)
-      time_stamp = re.search(time_stamp_pattern, one_line)
+    time_stamp = re.search(time_stamp_pattern, one_line)
     if time_stamp:
       temp_dict['time_stamp'] = time_stamp.group(0)
-      request = re.search(request_pattern, one_line)
+    request = re.search(request_pattern, one_line)
     if request:
       temp_dict['request'] = request.group(0)
     output.append(temp_dict)
@@ -22,7 +22,7 @@ def logtolist(filename):
 
 def re_logtolist(filename):
   output = [ ]
-  ip_address_pattern = re.compile(r'^\d{2}.\d{2,3}.\d{2,3}.\d{2}')
+  ip_address_pattern = re.compile(r'^\d{2}.\d{2,3}.\d{2,3}.\d{2,3}')
   time_stamp_pattern = re.compile(r'(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})')
   request_pattern = re.compile(r'GET\s([^"]+)')
   for one_line in filename:
@@ -30,10 +30,10 @@ def re_logtolist(filename):
     ip_address = re.match(ip_address_pattern, one_line)
     if ip_address:
       temp_dict['ip_address'] = ip_address.group(0)
-      time_stamp = re.search(time_stamp_pattern, one_line)
+    time_stamp = re.search(time_stamp_pattern, one_line)
     if time_stamp:
       temp_dict['time_stamp'] = time_stamp.group(0)
-      request = re.search(request_pattern, one_line)
+    request = re.search(request_pattern, one_line)
     if request:
       temp_dict['request'] = request.group(0)
     output.append(temp_dict)
