6baae1364260c513ba46a50b15e4cc415a761ce0
omarRmalik <maliko@mail.gvsu.edu>
2023-02-23 17:21:34 -0500

    Exercise 3 solved 2-23-23

--- Exercise 3/solution.py
+++ Exercise 3/solution.py
@@ -10,12 +10,18 @@ def logtolist(filename):
     ip_address = re.match(ip_address_pattern, one_line)
     if ip_address:
       temp_dict['ip_address'] = ip_address.group(0)
+    else:
+      return f'No IP address found'
     time_stamp = re.search(time_stamp_pattern, one_line)
     if time_stamp:
-      temp_dict['time_stamp'] = time_stamp.group(0)
+      temp_dict['timestamp'] = time_stamp.group(0)
+    else:
+      return f'No timestamp found'
     request = re.search(request_pattern, one_line)
     if request:
       temp_dict['request'] = request.group(0)
+    else:
+      return f'No request found'
     output.append(temp_dict)
   return output
 
@@ -30,11 +36,18 @@ def re_logtolist(filename):
     ip_address = re.match(ip_address_pattern, one_line)
     if ip_address:
       temp_dict['ip_address'] = ip_address.group(0)
+    else:
+      return f'No IP address found'
     time_stamp = re.search(time_stamp_pattern, one_line)
     if time_stamp:
-      temp_dict['time_stamp'] = time_stamp.group(0)
+      temp_dict['timestamp'] = time_stamp.group(0)
+    else:
+      return f'No timestamp found'
     request = re.search(request_pattern, one_line)
     if request:
       temp_dict['request'] = request.group(0)
+    else:
+      return f'No request found'
     output.append(temp_dict)
   return output
+
