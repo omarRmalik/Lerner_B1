eeee5ac19b7f0b8e2e04871c3cf30c815a96dc6b
omarRmalik <maliko@mail.gvsu.edu>
2023-03-01 10:30:41 -0500

    2nd commit Ex 4

--- Exercise4/solution.py
+++ Exercise4/solution.py
@@ -1,5 +1,6 @@
 import re
 from operator import itemgetter
+import arrow
 
 class LogDicts:
     def __init__(self, filename):
@@ -7,10 +8,44 @@ class LogDicts:
             self.infile = filename.read()
 
     def dicts(self, key=None):
-        pass
+        if key is None:
+            return [self.line_to_dict(line) for line in self.infile]
+        else:
+            return sorted([self.line_to_dict(line) for line in self.infile], key=key)
+
+    def line_to_dict(self,line):
+        if not line.strip():
+            return {}
+
+        regexp = r'''
+        ((?:\d{1,3}\.){3}\d{1,3})       # IP addresses contain four numbers (each with 1-3 digits)
+        .*                              # Junk between IP address and timestamp
+        \[([^\]]+)\]                    # Timestamp, defined to be anything between [ and ]
+        .*                              # Junk between timestamp and request
+        "(GET[^"]+)"                    # Request, starting with GET
+        '''
+        m = re.search(regexp, line, re.X)
+
+        if m:
+            ip_address = m.group(1)
+            timestamp = m.group(2)
+            request = m.group(3)
+
+        else:
+            ip_address = 'No IP address found'
+            timestamp = 'No timestamp found'
+            request = 'No request found'
+
+        output = {'ip_address': ip_address,
+                  'timestamp': timestamp,
+                  'request': request}
+        return output
 
     def iterdicts(self, key=None):
-        pass
+        if key is None:
+            return iter(self.dicts(key=None))
+        else:
+            return iter(self.dicts(key=key))
 
     def earliest(self, key=None):
         pass
