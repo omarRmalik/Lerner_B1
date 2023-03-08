78f2a9ff0ab585ef3c933a2fc23dc199f42248ea
omarRmalik <maliko@mail.gvsu.edu>
2023-03-08 10:22:05 -0500

    1st commit for Exercise 5 3-8-23

---
+++ Exercise5/solution.py
@@ -0,0 +1,18 @@
+import requests
+import csv
+
+gist_url = ' https://gist.githubusercontent.com/reuven/77edbb0292901f35019f17edb9794358/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+
+
+def cities_to_csv(url, filename):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        infile = response.json()
+
+        with open(filename, 'w') as outfile:
+            file_writer = csv.writer(outfile, delimiter='\t')
+            for one_dict in infile:
+                file_writer.writerow([one_dict['city'], one_dict['state'], one_dict['population'], one_dict['rank']])
+    else:
+        print('Request failed with status cose{response.status_code}')
