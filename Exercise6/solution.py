95f0d40b4056caedf3194f566551b3b1e2618b5a
omarRmalik <maliko@mail.gvsu.edu>
2023-03-14 16:22:12 -0400

    Exercise 6 first commit

---
+++ Exercise6/solution.py
@@ -0,0 +1,6 @@
+
+
+all_people = [{'name':'Reuven', 'age':50, 'hobbies':['Python', 'cooking', 'reading']},
+                  {'name':'Atara', 'age':20, 'hobbies':['horses', 'cooking', 'art', 'reading']},
+                  {'name':'Shikma', 'age':18, 'hobbies':['Python', 'piano', 'cooking', 'reading']},
+                  {'name':'Amotz', 'age':15, 'hobbies':['boxing', 'cooking']}]
