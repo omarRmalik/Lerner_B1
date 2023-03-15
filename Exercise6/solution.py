77ff23fcc460d7d1b434f81b1277cf7b945d02ff
omarRmalik <maliko@mail.gvsu.edu>
2023-03-15 10:04:34 -0400

    Exercise 6 2nd commit

--- Exercise6/solution.py
+++ Exercise6/solution.py
@@ -4,3 +4,15 @@ all_people = [{'name':'Reuven', 'age':50, 'hobbies':['Python', 'cooking', 'readi
                   {'name':'Atara', 'age':20, 'hobbies':['horses', 'cooking', 'art', 'reading']},
                   {'name':'Shikma', 'age':18, 'hobbies':['Python', 'piano', 'cooking', 'reading']},
                   {'name':'Amotz', 'age':15, 'hobbies':['boxing', 'cooking']}]
+
+def average_age_under(people, limit):
+    pass
+
+def all_hobbies(people):
+    pass
+
+def hobby_counter(people):
+    pass
+
+def n_most_common(people):
+    pass
