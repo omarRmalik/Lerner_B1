5ac6a6b4232e18aee97a7f14addc8f06c342e6f6
omarRmalik <maliko@mail.gvsu.edu>
2023-03-15 18:40:26 -0400

    4st commit for Exercise 5 3-8-23

--- Exercise6/solution.py
+++ Exercise6/solution.py
@@ -1,9 +1,10 @@
 from collections import Counter
 
-all_people = [{'name':'Reuven', 'age':50, 'hobbies':['Python', 'cooking', 'reading']},
-                  {'name':'Atara', 'age':20, 'hobbies':['horses', 'cooking', 'art', 'reading']},
-                  {'name':'Shikma', 'age':18, 'hobbies':['Python', 'piano', 'cooking', 'reading']},
-                  {'name':'Amotz', 'age':15, 'hobbies':['boxing', 'cooking']}]
+all_people = [{'name': 'Reuven', 'age': 50, 'hobbies': ['Python', 'cooking', 'reading']},
+              {'name': 'Atara', 'age': 20, 'hobbies': ['horses', 'cooking', 'art', 'reading']},
+              {'name': 'Shikma', 'age': 18, 'hobbies': ['Python', 'piano', 'cooking', 'reading']},
+              {'name': 'Amotz', 'age': 15, 'hobbies': ['boxing', 'cooking']}]
+
 
 def average_age_under(people, limit):
     return sum([one_person['age']
@@ -11,12 +12,18 @@ def average_age_under(people, limit):
                 if one_person['age'] >= 1 and
                 one_person['age'] <= limit]) / len(people)
 
+
 def all_hobbies(people):
-    return set(one_person['hobbies']
-                for one_person in people)
+    return set(one_hobby
+               for one_person in people
+               for one_hobby in one_person['hobbies'])
+
 
 def hobby_counter(people):
-    return Counter(people['hobbies'])
+    return Counter([one_hobby
+                    for one_person in people
+                    for one_hobby in one_person['hobbies']])
 
-def n_most_common(people):
-    return Counter(people['hobbies']).most_common(3)
+
+def n_most_common(people, n):
+    return [one_tuple[0] for one_tuple in hobby_counter(people).most_common(n)]
