812fbb82a51b7c906f82d49601c92c979ffdaf78
omarRmalik <maliko@mail.gvsu.edu>
2023-03-17 09:53:21 -0400

    Exercise 6 final commit 3-17-23

--- Exercise6/solution.py
+++ Exercise6/solution.py
@@ -1,27 +1,30 @@
 from collections import Counter
 from statistics import mean
 
-all_people = [{'name':'Reuven', 'age':50, 'hobbies':['Python', 'cooking', 'reading']},
-                  {'name':'Atara', 'age':20, 'hobbies':['horses', 'cooking', 'art', 'reading']},
-                  {'name':'Shikma', 'age':18, 'hobbies':['Python', 'piano', 'cooking', 'reading']},
-                  {'name':'Amotz', 'age':15, 'hobbies':['boxing', 'cooking']}]
+all_people = [{'name': 'Reuven', 'age': 50, 'hobbies': ['Python', 'cooking', 'reading']},
+              {'name': 'Atara', 'age': 20, 'hobbies': ['horses', 'cooking', 'art', 'reading']},
+              {'name': 'Shikma', 'age': 18, 'hobbies': ['Python', 'piano', 'cooking', 'reading']},
+              {'name': 'Amotz', 'age': 15, 'hobbies': ['boxing', 'cooking']}]
 
 def average_age_under(people, limit):
-    filtered_list =  [one_person['age']
-                      for one_person in people
-                      if one_person['age'] >= 1 and
-                      one_person['age'] <= limit]
+    filtered_list = [one_person['age']
+                     for one_person in people
+                     if one_person['age'] >= 1 and
+                     one_person['age'] <= limit]
     if filtered_list:
-      return mean(filtered_list)
+        return mean(filtered_list)
     else:
-      return 0
+        return 0
 
 def all_hobbies(people):
-    return set(one_person['hobbies']
-                for one_person in people)
+    return set(one_hobby
+               for one_person in people
+               for one_hobby in one_person['hobbies'])
 
 def hobby_counter(people):
-    return Counter(people['hobbies'])
+    return Counter([one_hobby
+                    for one_person in people
+                    for one_hobby in one_person['hobbies']])
 
 def n_most_common(people, n):
-    return Counter(people['hobbies']).most_common(n)
+    return sorted([one_tuple[0] for one_tuple in hobby_counter(people).most_common(n)])
