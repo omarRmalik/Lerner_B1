c67390ed5306e65ad54a61c1730ebd5252b3296a
omarRmalik <maliko@mail.gvsu.edu>
2023-03-22 16:26:24 -0400

    Exercise 7 First commit

--- Exercise7/test_solution.py
+++ Exercise7/test_solution.py
@@ -7,16 +7,14 @@ def test_filefunc():
 
     # Create a temporary directory and some files to test with
     os.makedirs("temp_dir")
-    os.makedirs("temp_dir/subdir")
     open("temp_dir/file1.txt", "w").close()
     open("temp_dir/file2.txt", "w").close()
-    open("temp_dir/subdir/file3.txt", "w").close()
 
     # Call filefunc with the temporary directory and mock function
     func_dict, excep_dict = filefunc("temp_dir", mock_func)
 
     # Assert that the function returned the correct values for each file
-    assert func_dict == {"file1.txt": 15, "file2.txt": 15, "file3.txt": 19}
+    assert func_dict == {"file1.txt": 15, "file2.txt": 15}
 
     # Assert that no exceptions were raised
     assert len(excep_dict) == 0
@@ -24,6 +22,4 @@ def test_filefunc():
     # Clean up the temporary directory
     os.remove("temp_dir/file1.txt")
     os.remove("temp_dir/file2.txt")
-    os.remove("temp_dir/subdir/file3.txt")
-    os.rmdir("temp_dir/subdir")
     os.rmdir("temp_dir")
