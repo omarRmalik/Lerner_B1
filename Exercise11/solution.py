027a8635de24d1ea9ddbc1272a2a982cf934668f
omarRmalik <maliko@mail.gvsu.edu>
2023-04-19 14:11:38 -0400

    Exercise 11 First commit

---
+++ Exercise11/solution.py
@@ -0,0 +1,17 @@
+import hashlib
+from pathlib import Path
+
+class DirFileHash(object):
+  def __init__(self, directory):
+    h = hashlib.new('md5')
+    self.dirname = h.update(directory.encode())
+    self.directory = Path(directory)
+    self.data = { }
+    for file_path in self.directory.iterdir():
+      if file_path.is_file():
+        with open(file_path, 'rb') as infile:
+          data = infile.read()
+          self.data[file_path] = hashlib.md5(data).hexdigest()
+
+  def __getitem__(self, key):
+        return self.data[key]
