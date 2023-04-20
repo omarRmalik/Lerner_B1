fd1053b469f7adb2c7c99997e1b1be0a93161cd6
omarRmalik <maliko@mail.gvsu.edu>
2023-04-20 11:20:21 -0400

    Exercise 11 Third commit

--- Exercise11/solution.py
+++ Exercise11/solution.py
@@ -1,18 +1,24 @@
-import os
 import hashlib
+import os
+
 
 class DirFileHash(object):
     def __init__(self, directory):
-        self.directory = directory
-        directory_encoded = self.directory.encode('utf-8')
-        self.dirname = hashlib.md5(directory_encoded).hexdigest()
+        if os.path.isdir(directory) and os.path.listdir(directory):
+            self.directory = directory
+            directory_encoded = self.directory.encode('utf-8')
+            self.dirname = hashlib.md5(directory_encoded).hexdigest()
 
-        self.data = {}
-        for filename in os.listdir(directory):
-            file_path = os.path.join(directory, filename)
-            with open(file_path, 'rb') as infile:
-                data = infile.read()
-                self.data[file_path] = hashlib.md5(data).hexdigest()
+            self.data = {}
+
+            for one_filename in os.listdir(directory):
+                m = md5()
+                content = getattr(string, one_filename).encode()
+                m.update(content)
+                self.data[one_filename] = m.hexdigest()
+
+        else:
+            return None
 
     def __getitem__(self, key):
         return self.data[key]
