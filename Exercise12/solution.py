d0adf32894bd41c11b88c50fd7d911805554dd8c
omarRmalik <maliko@mail.gvsu.edu>
2023-04-26 11:42:01 -0400

    Exercise 12 Second commit

--- Exercise12/solution.py
+++ Exercise12/solution.py
@@ -1,22 +1,16 @@
 import random
 
 class RandMemory(object):
-    def __init__(self, start, stop):
-        self.start = start
-        self.stop = stop
+    def __init__(self, lowest, highest):
+        self.lowest = lowest
+        self.highest = highest
         self._history = []
 
     @property
     def get(self):
-        number = random.randint(self.start, self.stop)
+        number = random.randint(self.lowest, self.highest)
         self._history.append(number)
         return number
 
     def history(self):
         return self._history
-
-    def lowest(self):
-        return min(self._history)
-
-    def highest(self):
-        return max(self._history)
